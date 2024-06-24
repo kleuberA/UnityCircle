ada29b80e4fdedf630105e0a41d16a2c5b84db66
Kleuber Silva <kleuber20@hotmail.com>
2024-06-24 15:00:17 -0300

    "Added Register component and integrated it with GraphQL mutation for user registration"

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Login from "@/components/auth/login";
+import Register from "@/components/auth/register";
 
 export default function Home() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
